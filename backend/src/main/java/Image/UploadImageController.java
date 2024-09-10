d103602db4ae4d18f8192001eadac67a7802f0b8
carlossiq <31148477+carlossiq@users.noreply.github.com>
2024-09-09 22:54:53 -0300

    upload de imagens com link atrelado atualizado

--- backend/src/main/java/Image/UploadImageController.java
+++ backend/src/main/java/Image/UploadImageController.java
@@ -1,22 +1,21 @@
 package Image;
 
-import java.io.IOException;
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.UUID;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import java.io.IOException;
+
 @RestController
-@RequestMapping("/api/images")
+@RequestMapping("/images")
 public class UploadImageController {
 
     @Autowired
@@ -43,5 +42,16 @@ public class UploadImageController {
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+    }
+
+    @GetMapping("/show")
+    public List<ImageIdDTO> getAllImageIds() {
+        return imageService.getAllImageIds().stream()
+            .map(id -> {
+                ImageIdDTO dto = new ImageIdDTO(id);
+                dto.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(UploadImageController.class).getImage(id)).withSelfRel());
+                return dto;
+            })
+            .collect(Collectors.toList());
     }
 }
