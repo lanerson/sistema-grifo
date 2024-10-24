85cc1249d94af1c621f558d6c6b8be01795b92a2
lanerson <lanerson153@gmail.com>
2024-10-24 09:46:35 -0300

    conexão com whatsapp e popup prontos

--- backend/src/main/resources/static/js/loja.js
+++ backend/src/main/resources/static/js/loja.js
@@ -1,3 +1,26 @@
+const popupForm = document.getElementById("popupForm");
+const closeBtn = document.getElementById("closeBtn");
+const encomendarForm = document.getElementById("orderForm");
+let productSelected=""
+closeBtn.addEventListener("click", () => {
+  popupForm.style.display = "none";
+});
+
+// Fechar o pop-up se clicar fora do formulário
+window.addEventListener("click", (event) => {
+  if (event.target === popupForm) {
+      popupForm.style.display = "none";
+  }
+});
+orderForm.addEventListener("submit", function(event) {
+  event.preventDefault(); // Impede o envio padrão do formulário  
+  // Captura os dados do formulário
+  const formData = new FormData(orderForm);
+  sendWhatsAppMessage(formData);
+  
+});
+
+
 fetch('http://localhost:8080/products/all', { method: 'GET' })
   .then(response => {
     if (!response.ok) {
@@ -7,8 +30,7 @@ fetch('http://localhost:8080/products/all', { method: 'GET' })
   })
   .then(data => {
     const products = data._embedded.productList; 
-    const productList = document.getElementById('product-list');
-    console.log(products)
+    const productList = document.getElementById('product-list');    
     // Limpa a lista antes de adicionar novos produtos
     productList.innerHTML = '';
 
@@ -35,9 +57,9 @@ fetch('http://localhost:8080/products/all', { method: 'GET' })
       button.type = 'button';
       button.className = 'button-encomendar encomendarBtn';
       button.textContent = 'Encomendar';
-      button.onclick = function() {
-        // Ação ao clicar no botão
-        alert(`Você encomendou: ${product.name}`);
+      button.onclick = function() {         
+        productSelected = product.name
+        popupForm.style.display = "flex";
       };
 
       // Adiciona todos os elementos à div do produto
@@ -53,3 +75,17 @@ fetch('http://localhost:8080/products/all', { method: 'GET' })
   .catch(error => {
     console.error('Erro:', error); // Trata erros
   });
+
+function sendWhatsAppMessage(formData) {  
+  var message = `Olá, meu nome é ${formData.get('nome')}.\n` + 
+                    `Telefone: ${formData.get('email')}.\n` + 
+                    `Sou sócio: ${formData.get('socio') === 'sim' ? 'Sim' : 'Não'}.\n` + 
+                    `Gostaria de fazer uma encomenda deste produto: \n` +
+                    productSelected+`\n`+
+                    `Tamanho: ${formData.get('tamanho')}`
+
+  console.log(message)
+  var whatsappLink = `https://api.whatsapp.com/send?phone=+558591893009&text=${encodeURIComponent(message)}`;
+
+  window.open(whatsappLink, '_blank'); // Abre o WhatsApp em uma nova aba
+}
--- frontend/scripts/loja.js
+++ frontend/scripts/loja.js
@@ -1,22 +1,22 @@
 const encomendarBtns = document.querySelectorAll(".encomendarBtn");
-        const popupForm = document.getElementById("popupForm");
-        const closeBtn = document.getElementById("closeBtn");
+const popupForm = document.getElementById("popupForm");
+const closeBtn = document.getElementById("closeBtn");
 
-        // Adiciona um evento de clique a cada botão "ENCOMENDAR"
-        encomendarBtns.forEach(button => {
-            button.addEventListener("click", () => {
-                popupForm.style.display = "flex";
-            });
-        });
+// Adiciona um evento de clique a cada botão "ENCOMENDAR"
+encomendarBtns.forEach(button => {
+    button.addEventListener("click", () => {
+        popupForm.style.display = "flex";
+    });
+});
 
-        // Fechar o formulário ao clicar no botão "X"
-        closeBtn.addEventListener("click", () => {
-            popupForm.style.display = "none";
-        });
+// Fechar o formulário ao clicar no botão "X"
+closeBtn.addEventListener("click", () => {
+    popupForm.style.display = "none";
+});
 
-        // Fechar o pop-up se clicar fora do formulário
-        window.addEventListener("click", (event) => {
-            if (event.target === popupForm) {
-                popupForm.style.display = "none";
-            }
-        });
+// Fechar o pop-up se clicar fora do formulário
+window.addEventListener("click", (event) => {
+    if (event.target === popupForm) {
+        popupForm.style.display = "none";
+    }
+});
